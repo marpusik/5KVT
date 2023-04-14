13f81f02f206a2b33addde37eb3c03c8bad95599
marpusik <marpusik1987@gmail.com>
2023-04-14 13:58:19 +0300

    07

--- src/js/main.js
+++ src/js/main.js
@@ -25,9 +25,9 @@ function burgerMenu() {
 burgerMenu()
 const swiper = new Swiper('.swiper', {
   // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
