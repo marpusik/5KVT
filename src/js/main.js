581d247cce9f23415cfa061e1e31e5e78139f576
marpusik <marpusik1987@gmail.com>
2023-05-23 10:21:22 +0300

    27

--- src/js/main.js
+++ src/js/main.js
@@ -1,85 +1,105 @@
 function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
-    } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
-  // Вот тут мы ставим брейкпоинт навбара
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 991.98) {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
 }
+
 burgerMenu()
 const swiper = new Swiper('.swiper__first', {
-  slidesPerView: 1,
-  // spaceBetween: 10,
-  breakpoints: {
-    992: {
-      slidesPerView: 1,
-      // spaceBetween: 10,
-    }
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+        992: {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+        }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 const swiper2 = new Swiper('.swiper__recently', {
-  slidesPerView: 1,
-  // spaceBetween: 10,
-  breakpoints: {
-    992: {
-      slidesPerView: 1,
-      // spaceBetween: 10,
-    }
-  },
-  navigation: {
-    nextEl: '.next__btn',
-    prevEl: '.prev__btn',
-  },
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+        992: {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+        }
+    },
+    navigation: {
+        nextEl: '.next__btn',
+        prevEl: '.prev__btn',
+    },
 });
 const swiper3 = new Swiper('.swiper__edge', {
-  slidesPerView: 1,
-  // spaceBetween: 10,
-  breakpoints: {
-    992: {
-      slidesPerView: 1,
-      // spaceBetween: 10,
-    }
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+        992: {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+        }
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 });
+const swiper4 = new Swiper('.swiper__catalog', {
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+        992: {
+            slidesPerView: 1,
+            // spaceBetween: 10,
+        }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
 function accordion() {
-  const items = document.querySelectorAll('.accordion__item-trigger')
-  items.forEach(item => {
-    item.addEventListener('click', () => {
-      const parent = item.parentNode
-      if (parent.classList.contains('accordion__item-active')) {
-        parent.classList.remove('accordion__item-active')
-      } else {
-        document
-            .querySelectorAll('.accordion__item')
-            .forEach(child => child.classList.remove('accordion__item-active'))
-        parent.classList.add('accordion__item-active')
-      }
+    const items = document.querySelectorAll('.accordion__item-trigger')
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            if (parent.classList.contains('accordion__item-active')) {
+                parent.classList.remove('accordion__item-active')
+            } else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))
+                parent.classList.add('accordion__item-active')
+            }
+        })
     })
-  })
 }
+
 accordion()
 
 
